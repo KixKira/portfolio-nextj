48265cb0e3b520814f7bb114a6ad5d567bce93d0
Oscar Machado <plus44b@hotmail.com>
2024-08-10 12:55:25 -0400

    Mo

--- components/About.jsx
+++ components/About.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const About = () => {
   return (
-    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
+    <div id="about" className="w-full  p-2 flex items-center py-16">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
           <p className="uppercase text-xl tracking-widest text-[#5651e5]">
@@ -31,9 +31,6 @@ const About = () => {
               Take a look at some of my latest projects.
             </p>
           </Link>
-        </div>
-        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-          {/* <Image src={AboutImg} className="rounded-xl" alt="/" /> */}
         </div>
       </div>
     </div>
--- components/Main.jsx
+++ components/Main.jsx
@@ -6,8 +6,8 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Main = () => {
   return (
-    <div id="home" className="w-full h-screen text-center">
-      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+    <div id="home" className="w-full h-full text-center">
+      <div className="max-w-[1240px] w-full h-[700px] mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-600">
             LET&apos;S BUILD SOMETHING TOGETHER
